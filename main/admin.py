2693003d5c0c303088a72150f936cd5c88a401c5
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-01-23 13:48:04 +0300

    Add models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from main.models import Student, Teacher, Subject, Mark
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Subject)
+admin.site.register(Mark)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
+
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=60, default="Name")
+    surname = models.CharField(max_length=60, default="Surname")
+    patronymic = models.CharField(max_length=60, default="Patronymic")
+
+    def __str__(self):
+        return self.surname + " " + self.name + " " + self.patronymic
+
+
+class Student(Person):
+    year = models.IntegerField(default=1)
+
+
+class Teacher(Person):
+    pass
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=60, default="Subject")
+
+    def __str__(self):
+        return self.name
+
+
+class Mark(models.Model):
+    value = models.IntegerField()
+    date = models.DateField()
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, null=True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, null=True)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, null=True)
+
+
