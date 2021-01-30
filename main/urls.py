54d5265bf243a779d3964b7d4d8c9f28bef07b03
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2021-01-30 11:40:06 +0300

    Add login & register form

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="home"),
+    path('login/', views.loginPage, name="login"),
+    path('register/', views.registerPage, name="register"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,14 +1,36 @@
+from django.contrib.auth import authenticate
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from main.forms import CreateUserForm
 
 
-# Create your views here.
 def index(request):
-
     context = {
         "values": ["Test 1", "Test 2"]
     }
-
     return render(request, "main/index.html", context)
 
-# templates/main/index.html
+
+def loginPage(request):
+    return render(request, "main/login.html")
+
+
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password1")
+            authenticate(request, username=username, password=password)
+            return redirect('home')
+
+    context = {
+        "form": form
+    }
+    return render(request, "main/register.html", context)
